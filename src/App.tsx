60e6e43fba42310cb66eaa6c59a626f96d0cfc1d
dkanskcj <dkanskcj@likelion.org>
2023-01-09 00:14:08 +0900

    feat(routes): install react router dom, mk 404 ...

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,17 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import Error404Page from "@components/error/Error404Page";
+import Home from "@components/home/Home";
+import Path from "@utils/path";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div>
+      <Routes>
+        <Route path={Path.HOME} element={<Home />} />
+        <Route path={Path.ERR404} element={<Error404Page />} />
+        <Route path="*" element={<Navigate replace to={Path.ERR404} />} />
+      </Routes>
     </div>
   )
 }
---
+++ src/components/error/Error404Page.tsx
@@ -0,0 +1,12 @@
+
+
+const Error404Page = () => {
+  return (
+    <div className="text-7xl">
+      <h1>404</h1>
+      <p className="uppercase">Not Found</p>
+    </div>
+  )
+}
+
+export default Error404Page
