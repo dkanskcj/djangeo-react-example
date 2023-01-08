151f1dec58eba33d6f1af1a0fa539ca4e5853720
dkanskcj <dkanskcj@likelion.org>
2023-01-09 01:55:04 +0900

    fix(auth): add otp and fix relatives

--- src/store/auth/useAuth.ts
+++ src/store/auth/useAuth.ts
@@ -3,6 +3,11 @@ import create from "zustand";
 
 interface AuthState {
   isAuthenticated: boolean;
+  isAuthInfoConfirmed: boolean;
+  isOTPConfirmed: boolean;
+  
+  confirmAuthInfo: (authInfo: AuthInfo) => void;
+  confirmOTP: (otp: string) => void;
   
   login: (authInfo: AuthInfo) => void;
   logout: () => void;
@@ -11,10 +16,19 @@ interface AuthState {
 const useAuth = create<AuthState>((set, get) => {
   return {
     isAuthenticated: false, // 웹스토리지나 서버에서 인증 여부 가져와서 초깃값 담아도 됨.
+    isAuthInfoConfirmed: false,
+    isOTPConfirmed: false,
 
-    login: (authInfo) => {
-      const isAuthenticated = true; // Pass
-      set({ isAuthenticated });
+    confirmAuthInfo: (authInfo) => {
+      set({isAuthInfoConfirmed: true});
+    },
+
+    confirmOTP: (otp) => {
+      set({isOTPConfirmed: true});
+    },
+
+    login: () => {
+      set({ isAuthenticated: true });
     },
 
     logout: () => {
